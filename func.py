46836fe34e973074f6c55ce1848b2f9dc9b92e19
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-02 23:48:01 +0530

    Create func.py

---
+++ func.py
@@ -0,0 +1,15 @@
+def myFunction() :
+  return True
+
+print(myFunction())
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+def myFunction() :
+  return True
+
+print(myFunction()
